d629090fec04aecdf4d9369a1e373c8fc94d0576
melaya-j <melaya710@gmail.com>
2024-11-04 14:33:12 -0600

    finished login test cases

--- test/validateLogin.test.js
+++ test/validateLogin.test.js
@@ -9,5 +9,13 @@ describe('validateLogin', ()=> {
     it('should return true for correct username and correct password', ()=> {
         expect (validateLogin("correct username", "correct password")).to.be.true;
     });
-
+    it('should return false for incorrect username and correct password' , ()=> {
+        expect(vaildateLogin("correct username" , "correct passowrd")).to.be.false;
+    })
+    it('should return false for correct username and incorrect password' , ()=> {
+        expect (validateLogin("correct username" , "correct password")).to.be.false;
+    })
+    it('should return false for incorrect username and incorrect password' , ()=> {
+        expect (validateLogin("correct username", "correct password" )).to.be.false;
+    })
 });
